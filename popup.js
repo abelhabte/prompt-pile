cd705c41cd27a36bf0e58e895251a434544ee629
abelhabte <132626177+abelhabte@users.noreply.github.com>
2026-01-22 23:04:03 -0500

    Updated popup.js
    Updated popup.js

--- popup.js
+++ popup.js
@@ -1,6 +1,29 @@
-// Function for clicking on + Folder button
-const addFolder = document.getElementById(add-folder-btn);
+// Added quotes around the ID strings
+const addFolder = document.getElementById("add-folder-btn"); 
+const mainListView = document.getElementById("main-list-view");
 
 addFolder.addEventListener("click", () => {
-    const newFolder = document.createElement("div");
+    const folder = document.createElement("div");
+    folder.className = 'folder';
+    
+    folder.innerHTML = `
+        <div class="icon">📂</div>
+        <input type="text" class="folder-input" placeholder="Folder Name..." />
+    `;
+
+    // Only need to append once
+    mainListView.appendChild(folder);
+    
+    const input = folder.querySelector('.folder-input');
+    input.focus();
+
+    input.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            const folderName = input.value || "Untitled Folder";
+            folder.innerHTML = `
+                <div class="icon">📂</div>
+                <span class="folder-name">${folderName}</span>
+            `;
+        }
+    });
 });
