dc5b01a010a2945b4047a0b258e6f2f51e6485eb
abelhabte <132626177+abelhabte@users.noreply.github.com>
2026-01-22 20:10:34 -0500

    Updated files
    Updated popup.html and popup.js

---
+++ popup.js
@@ -0,0 +1,6 @@
+// Function for clicking on + Folder button
+const addFolder = document.getElementById(add-folder-btn);
+
+addFolder.addEventListener("click", () => {
+    const newFolder = document.createElement("div");
+});
