b126f8540ac8f58a0b365babfc0f00551c024d4f
abelhabte <132626177+abelhabte@users.noreply.github.com>
2026-04-07 20:46:45 -0400

    Updated background.js
    Updated background.js

--- background.js
+++ background.js
@@ -1,3 +1,15 @@
-chrome.sidePanel
-  .setPanelBehavior({ openPanelOnActionClick: true })
-  .catch((error) => console.error(error));
+// --- Chrome Logic ---
+if (typeof chrome !== "undefined" && chrome.sidePanel) {
+    chrome.sidePanel
+        .setPanelBehavior({ openPanelOnActionClick: true })
+        .catch((error) => console.error(error));
+}
+
+// --- Firefox Logic ---
+// In Firefox, we listen for the 'onClicked' event on the extension icon
+// and manually tell the browser to open the sidebar.
+if (typeof browser !== "undefined" && browser.sidebarAction) {
+    browser.action.onClicked.addListener(() => {
+        browser.sidebarAction.open();
+    });
+}
