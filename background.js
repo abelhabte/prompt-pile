38559a27802627a59bb81e73046749dc2713e84b
abelhabte <132626177+abelhabte@users.noreply.github.com>
2026-04-14 18:42:08 -0400

    Update background.js
    Formatted background.js

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 if (typeof chrome !== "undefined" && chrome.sidePanel) {
-    chrome.sidePanel
-        .setPanelBehavior({ openPanelOnActionClick: true })
-        .catch((error) => console.error(error));
+  chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
 }
 
 if (typeof browser !== "undefined" && browser.sidebarAction) {
-    browser.action.onClicked.addListener(() => {
-        browser.sidebarAction.open();
-    });
+  browser.action.onClicked.addListener(() => {
+    browser.sidebarAction.open();
+  });
 }
